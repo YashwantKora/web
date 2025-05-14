03b12159b55b9ee336b37d285a2ffdbf1e31a8f7
YashwantKora <Yashwantqwerty@gmail.com>
2025-05-14 20:29:11 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log(`Hello`);
+console.log(`I like pizza`);
+
+//window.alert("DANGER!");
+//window.alert("HELLO");
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = "Brawl Stars idk"
+
+let x;
+let y = 20;
+x = 10;
+
+let age = 18;
+let price = 10.99;
+let gpa = 9.79;
+
+console.log(x);
+console.log(y);
+console.log(`You are ${age} years old!`);
+console.log(`The price is Rs.${price}`);
+console.log(`Your GPA is: ${gpa}`);
+console.log(typeof gpa);
