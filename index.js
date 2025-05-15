864902cd53f7c2b5eec903692fafd44e556060ee
YashwantKora <yashwantqwerty@gmail.com>
2025-05-15 19:16:32 +0530

    new commits

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-console.log(`Hello`);
-console.log(`I like pizza`);
+// console.log(`Hello`);
+// console.log(`I like pizza`);
 
-//window.alert("DANGER!");
-//window.alert("HELLO");
+// //window.alert("DANGER!");
+// //window.alert("HELLO");
 
-document.getElementById("myH1").textContent = `Hello`;
-document.getElementById("myP").textContent = "Brawl Stars idk"
+// document.getElementById("myH1").textContent = `Hello`;
+// document.getElementById("myP").textContent = "Brawl Stars idk"
 
-let x;
-let y = 20;
-x = 10;
+// let x;
+// let y = 20;
+// x = 10;
 
-let age = 18;
-let price = 10.99;
-let gpa = 9.79;
+// let age = 18;
+// let price = 10.99;
+// let gpa = 9.79;
+// let firstname = "Yashwant";
+// let online = true;
 
-console.log(x);
-console.log(y);
-console.log(`You are ${age} years old!`);
-console.log(`The price is Rs.${price}`);
-console.log(`Your GPA is: ${gpa}`);
-console.log(typeof gpa);
+// console.log(x);
+// console.log(y);
+// console.log(`You are ${age} years old!`);
+// console.log(`The price is Rs.${price}`);
+// console.log(`Your GPA is: ${gpa}`);
+// console.log(typeof gpa);
+// console.log(typeof firstname);
+// console.log(typeof online);
+
+
+// let username;
+// username = window.prompt("Enter The Username:");
+
+// document.getElementById("myH1").textContent = "Hello " + username + "!";
+
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    username = username.toUpperCase();
+    console.log(username);
+    document.getElementById("myH1").textContent = "Hello, " + username + "!";
+}
